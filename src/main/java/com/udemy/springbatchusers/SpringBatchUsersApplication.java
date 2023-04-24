19c081829fd599ddabef5a81ec645d074c820769
Sandro Livino <sandro.livino@gmail.com>
2023-04-24 17:24:39 -0300

    Commit inicial

---
+++ src/main/java/com/udemy/springbatchusers/SpringBatchUsersApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.springbatchusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchUsersApplication.class, args);
+	}
+
+}
